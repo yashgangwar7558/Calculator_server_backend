79af4bda6bf35eb694bfea346d661816f9a1c3ad
Yash Gangwar <gangwaryash06@gmail.com>
2023-08-26 21:46:24 +0530

    implemented /history and /history/all endpoint

--- src/routers/calculator.js
+++ src/routers/calculator.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 const History = require('../models/history');
 
 // to GET the result and store it in database
-router.get('/calculate/:expression(*)', (req, res) => {
+router.get('/calculate/:expression(*)', async (req, res) => {
     const expression = req.params.expression;
 
     // replace mathematical words to symbols
@@ -19,7 +19,7 @@ router.get('/calculate/:expression(*)', (req, res) => {
 
         // adding calculations history to database
         const addingCalculation = new History(operation);
-        const result = addingCalculation.save();
+        const result = await addingCalculation.save();
 
         res.status(201).json(operation);
     } catch (error) {
@@ -27,10 +27,79 @@ router.get('/calculate/:expression(*)', (req, res) => {
     }
 })
 
-// to GET the recent 20 Calculation History stored in database 
-router.get('/history', (req, res) => {
+
+// to GET the recent 20 Calculations History stored in database 
+router.get('/history', async (req, res) => {
     try {
-        res.send("History");
+        const getHistory = await History.find().sort({ createdAt: -1 }).limit(20)
+
+        const reversedHistory = getHistory.slice().reverse(); 
+
+        const htmlResponse = `
+            <html>
+                <head>
+                    <title>History</title>
+                </head>
+                <body>
+                    <h1>History</h1>
+                    <table>
+                        <tr>
+                            <th>Question</th>
+                            <th>Answer</th>
+                            <th>Done At</th>
+                        </tr>
+                        ${reversedHistory.map(item => `
+                            <tr>
+                                <td>${item.question}</td>
+                                <td>${item.answer}</td>
+                                <td>${item.createdAt}</td>
+                            </tr>
+                        `).join('')}
+                    </table>
+                </body>
+            </html>
+        `;
+
+        res.send(htmlResponse);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+})
+
+
+// to GET ALL Calculations History stored in database 
+router.get('/history/all', async (req, res) => {
+    try {
+        const getHistory = await History.find().sort({ createdAt: -1 })
+
+        const reversedHistory = getHistory.slice().reverse(); 
+
+        const htmlResponse = `
+            <html>
+                <head>
+                    <title>History</title>
+                </head>
+                <body>
+                    <h1>History</h1>
+                    <table>
+                        <tr>
+                            <th>Question</th>
+                            <th>Answer</th>
+                            <th>Done At</th>
+                        </tr>
+                        ${reversedHistory.map(item => `
+                            <tr>
+                                <td>${item.question}</td>
+                                <td>${item.answer}</td>
+                                <td>${item.createdAt}</td>
+                            </tr>
+                        `).join('')}
+                    </table>
+                </body>
+            </html>
+        `;
+
+        res.send(htmlResponse);
     } catch (err) {
         res.status(400).send(err);
     }
