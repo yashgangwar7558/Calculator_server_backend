19fb51b19ae60a93ba0ba929c2de6521df5509ad
Yash Gangwar <gangwaryash06@gmail.com>
2023-08-26 14:53:05 +0530

    established connection to mongodb

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+require("../src/db/conn");
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(express.json()); // to allow express to handle/use JSON data
+// to allow express to handle/use JSON data
+app.use(express.json()); 
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.get('/', async (req, res) => {
     res.send("Hello World!")
---
+++ src/db/conn.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config({ path: '../../.env' });
+
+const CONNECTION_URL = process.env.MONGODB_CONNECTION_URL;
+
+console.log(CONNECTION_URL);
+
+if (!CONNECTION_URL) {
+    console.error("Error: MONGODB_CONNECTION_URL environment variable is not set.");
+} else {
+    mongoose.connect(CONNECTION_URL,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    ).then((res) => {
+        console.log("Hurray!! Successfully Connected to the database");
+    }).catch((err) => {
+        console.log("Connection failed to the database!!");
+    });
+}
